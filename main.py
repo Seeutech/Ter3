8c10c2524dd0774b3db6b04e5af2cde596dec149
Mavi Mods <139610566+MaviMods@users.noreply.github.com>
2024-03-15 08:55:18 +0000

    Update main.py

--- main.py
+++ main.py
@@ -38,11 +38,11 @@ db = redis.Redis(
 @bot.on(events.NewMessage(pattern="/start$", incoming=True, outgoing=False))
 async def start(m: UpdateNewMessage):
     reply_text = f"""
- 𝐇𝐞𝐥𝐥𝐨! {mention} 𝐈 𝐚𝐦 𝐓𝐞𝐫𝐚𝐛𝐨𝐱 𝐕𝐢𝐝𝐞𝐨 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐞𝐫 𝐁𝐨𝐭.
+ 𝐇𝐞𝐥𝐥𝐨! {user_name} 𝐈 𝐚𝐦 𝐓𝐞𝐫𝐚𝐛𝐨𝐱 𝐕𝐢𝐝𝐞𝐨 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝𝐞𝐫 𝐁𝐨𝐭.
 𝐒𝐞𝐧𝐝 𝐦𝐞 𝐭𝐞𝐫𝐚𝐛𝐨𝐱 𝐯𝐢𝐝𝐞𝐨 𝐥𝐢𝐧𝐤 & 𝐈 𝐰𝐢𝐥𝐥 𝐬𝐞𝐧𝐝 𝐕𝐢𝐝𝐞𝐨.
 
 𝐏𝐋𝐀𝐍'𝐒 : /plans"""
-    mention = "["+user_name+"](tg://user?id="+str(user_id)+")"
+     user_name = message.from_user.first_name 
     # Check if the user is a member of both channels
     channel1 = "@mavimods2"
     channel2 = "@mavibot_support"  # Replace with the actual username of your second channel
