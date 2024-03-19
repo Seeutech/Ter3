74f7279dd157c1aa43c2e2348b2e89766b170c1d
Seeutech <118717438+Seeutech@users.noreply.github.com>
2024-03-19 17:53:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ BOT_TOKEN = "6758593032:AAGbmikx-sLqa0qO-eMM4d9WFwroDR6PB4A"  # bot token
 
 
 ## REDIS
-HOST = ""  # redis host uri
-PORT = ""  # redis port
-PASSWORD = ""  # redis password
+HOST = "redis-12475.c1.asia-northeast1-1.gce.cloud.redislabs.com"  # redis host uri
+PORT = "12475"  # redis port
+PASSWORD = "hkuQOvjFdcqoaq8BySscAhO1knX6Ep3Q"  # redis password
 
 PRIVATE_CHAT_ID = -1002095112878  # CHAT WHERE YOU WANT TO STORE VIDEOS
 COOKIE = ""  # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=; lang=en; TSID=; __bid_n=; _ga=; __stripe_mid=; ndus=; browserid==; ndut_fmt=; _ga_06ZNKL8C2E=" (dont use this)
