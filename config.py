bcbf996410eb1562da2755359e06f941dd5618fd
Seeutech <118717438+Seeutech@users.noreply.github.com>
2024-03-24 17:53:52 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,14 +1,4 @@
 API_ID = 23054736  # api id
 API_HASH = "d538c2e1a687d414f5c3dce7bf4a743c"  # api hash
 
-BOT_TOKEN = "7054995045:AAHr1AoUrXYQQZpJXiW1vPkWL0yuMCLfJFA"  # bot token
-
-
-## REDIS
-HOST = "redis-12475.c1.asia-northeast1-1.gce.cloud.redislabs.com"  # redis host uri
-PORT = 12475  # redis port
-PASSWORD = "hkuQOvjFdcqoaq8BySscAhO1knX6Ep3Q"  # redis password
-
-PRIVATE_CHAT_ID = -1002146782406  # CHAT WHERE YOU WANT TO STORE VIDEOS
-COOKIE = "PANWEB=1; csrfToken=BnQx-ffuJKamkWLrsx3Ksds4; browserid=qEGrcJcpudjTVSp34yywiOK8KGczvjGYTPI6jJMydd7dpoQVUXCzDlp7JFM=; lang=en; __bid_n=18db31bdad1e0ee8094207; __stripe_mid=a300821a-4b38-4e0a-a227-5c27f1b7c54789f06e; __stripe_sid=f7b244b5-18a8-4540-bfa0-222b4acc0bf5eb56ba; ndus=Yb47bKTteHuiwDuAJYscQHtlthgbg2-2R4yRRWFe; ndut_fmt=17DAF2FD0DC6DF723D10EB645D49FD8581EFDCCB4A2823D11359EA0CCE270FBE"  # (dont use this)
-ADMINS = [1352497419]
+BOT_TOKEN = "7182824305:AAEm7g28hhrwo4nBS9zEUK1NY-EOdD6Laa4"  # bot token
